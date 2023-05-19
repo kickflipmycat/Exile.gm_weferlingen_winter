87254257107e3f6cf0dfbd53da5a08e92a6625d5
kickflipmycat <132964682+kickflipmycat@users.noreply.github.com>
2023-05-19 22:32:25 +0930

    Revert "1"
    This reverts commit 48feec2ba2003d87f420341956b123b0b9be7b50.

--- config.cpp
+++ config.cpp
@@ -8690,8 +8690,6 @@ class CfgExileArsenal
 	class Exile_Item_ToiletPaper					{ quality = 1; price = 5; };
 	class Exile_Item_ZipTie						{ quality = 1; price = 250; };
 	class Exile_Item_WoodPlank						{ quality = 1; price = 10; };
-	class Exile_Item_FloodLightKit						{ quality = 1; price = 700; };
-	class Exile_Item_PortableGeneratorKit				{ quality = 1; price = 700; };
 
 	///////////////////////////////////////////////////////////////////////////////
 	// Navigation
@@ -14672,7 +14670,9 @@ class CfgTraderCategories
 			"Exile_Item_MetalHedgehogKit",
 			"Exile_Item_Cement",
 			"Exile_Item_Sand",
-			"Exile_Item_MobilePhone",
+			"Exile_Item_MobilePhone"
+			/*
+			--Unused--
 			"Exile_Item_SprayCan_Black",
 			"Exile_Item_SprayCan_Red",
 			"Exile_Item_SprayCan_Green",
@@ -14686,7 +14686,7 @@ class CfgTraderCategories
 			"Exile_Item_SleepingMat",
 			"Exile_Item_Defibrillator",
 			"Exile_Item_Wrench",
-			"Exile_Item_Rope"
+			"Exile_Item_Rope"*/
 							
 		};
 	};
@@ -14857,9 +14857,7 @@ class CfgTraderCategories
 			"gm_repairkit_01",
 			"gm_watch_kosei_80",
 			"Exile_Item_WoodPlank",
-			"Exile_Melee_Axe",
-			"Exile_Item_FloodLightKit",
-			"Exile_Item_PortableGeneratorKit"		
+			"Exile_Melee_Axe"			
 		};
 	};
 
